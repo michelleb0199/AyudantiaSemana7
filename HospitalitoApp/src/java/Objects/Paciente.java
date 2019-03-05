8bdb437421d8b66a7dd1c751fc22c5a5c4cd6bca
Michelle Beltrán <47165680+michelleb0199@users.noreply.github.com>
2019-03-05 10:49:23 -0600

    Update

---
+++ HospitalitoApp/src/java/Objects/Paciente.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objects;
+
+/**
+ *
+ * @author miche
+ */
+public class Paciente {
+    private String m_strName;
+    private String m_strLastName;
+    private String m_strGendere;
+    private String m_strSymptoms;
+    private int m_iBirthDay;
+    private int m_iBirthYear;
+    private int m_iBirth;
+    
+}
